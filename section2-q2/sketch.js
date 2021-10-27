995561b3df76aba7126c87f90a59e2883e380abd
All-Shadows <92131203+All-Shadows@users.noreply.github.com>
2021-10-27 15:45:11 +0900

    section2-2q update

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,58 +1,38 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width/8;
-  noStroke();
-for(let i = 0; i < 8; i++){
-  for(let j = 0; j < 8; j++){
+  let size=width/8;
 
-     if((i+j)%2==0){
-    fill(250);
+  for(let i=0; i<8; i++){
+    for(let j =0; j<8; j++){
 
-  }
-  if((i+j)%2==1){
-    fill(150)
 
-  }
-    rect(i * size, j * size,size, size);
-  }
-
-}
-
-  for(let r =0;  r<5; r++){
-    fill(255,0,0);
-  circle(37.5+50*(r-1),12,size);
-
+      if((i+j)%2==0){
+        fill(255);
+      }
+      else{
+        fill(128);
+      }
+      noStroke();
+      rect(size*i, size*j, size,size);
+    }
   }
 
 
-  for (let k=0;  k<5; k++){
-    fill(255,0,0);
-  circle(12.5+50*(k-1),37.5,size);
+
+  for(let i=0; i <8; i ++){
+    for(let j=0; j<8; j++){
+      if((i+j)%2==1&&j<3){
+        fill(255,0,0);
+        noStroke();
+        ellipse(size*i+size/2, size*j+size/2,size*0.85);
+        }
+      else if ((i+j)%2==1 && j>4){
+        fill(0);
+        noStroke();
+        ellipse(size*i+size/2, size*j+size/2,size*0.85);
+
+      }
+    }
   }
-
-
-   for(let s =0;  s<4; s++){
-    fill(255,0,0);
-  circle(37.5+50*(s-1),62.5,size);
-  }
-
-
-  for(let w =0;  w<5; w++){
-    fill(0);
-  circle(12.5+50*(w-1),137.5,size);
-  }
-
-
-   for(let h =0;  h<5; h++){
-    fill(0);
-  circle(37.5+50*(h-1),162.5,size);
-   }
-
-  for(let m =0;  m<5; m++){
-    fill(0);
-  circle(12.5+50*(m-1),187.5,size);
-   }
-
-
 }
