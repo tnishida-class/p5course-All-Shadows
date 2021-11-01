76aa4cc6267d151b87c8395ffc35d9130ec4cbfd
All-Shadows <92131203+All-Shadows@users.noreply.github.com>
2021-11-02 03:12:12 +0900

    section2-q4 update
    EU国旗

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,30 @@
-// ギリシャ国旗
+// EUの国旗
 function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
-  // noStroke();
-  background(255);
+  createCanvas(600, 400);
+  angleMode(DEGREES);
+  textSize(50)
+}
 
-  let d = height / 9; // 縞1本の太さ
+function draw() {
+  background(55, 52, 235);
+  fill(235, 192, 52);
+  translate(width / 2, height / 2);
 
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
+
+  for (let i = 0; i < 12; ++i) {
+    push();
+      var angle = 360 / 12 * i
+      rotate(angle);
+
+      translate(int(width / 5), 0);
+
+    push()
+
+       text("★", 0, 0)
+    pop()
+
+    pop();
   }
 
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
 
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
